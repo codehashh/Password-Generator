5ae1216ea93cd28ee60d3a7a0c787913c260bfe9
codehashh <hasdeep26singh@gmail.com>
2026-06-12 22:10:09 +0530

    Delete inspect_app.py

--- inspect_app.py
+++
@@ -1,6 +0,0 @@
-src = open('app.py', encoding='utf-8').read()
-print('len', len(src))
-print(repr(src[:400]))
-ns = {'__name__':'not_main'}
-exec(src, ns)
-print('symbols:', sorted([k for k in ns.keys() if not k.startswith('__')]))
